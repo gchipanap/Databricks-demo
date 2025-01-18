9bfad97507f7b48b371a17585021c07564dd9d21
gchipanap <gchipanap>
2025-01-18 02:10:06 +0000

    add notebooks

---
+++ (Clone) Demo2.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+print("holi")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "aaa" as abc
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC #HOoli
+# MAGIC ala
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %run ./Includes/Setup
